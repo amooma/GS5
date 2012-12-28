9190c997fe17ed1c680d2b073c8d2e21ac864b57
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2012-12-28 12:57:24 +0100

    Update some Phone attributes. Which where introduced with 9101b20cc5ca4e0553cfb4a503f0d2d51863ba37

--- db/migrate/20121228101454_update_tenant_id.rb
+++ db/migrate/20121228101454_update_tenant_id.rb
@@ -1,7 +1,11 @@
 class UpdateTenantId < ActiveRecord::Migration
   def up
-    Phone.where(:hot_deskable => true).each do |phone|
+  	Phone.all.each do |phone|
       phone.tenant_id = Tenant.last.id
+      phone.save
+  	end
+
+    Phone.where(:hot_deskable => true).each do |phone|
       phone.fallback_sip_account = phone.sip_accounts.where(:sip_accountable_type => 'Tenant').first
       phone.save
     end
