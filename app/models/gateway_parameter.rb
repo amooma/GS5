6ae1b0df2648ccc02afa8fe375693e15117736fa
spag <spag@golwen.net>
2013-01-13 20:33:35 +0100

    validations

--- app/models/gateway_parameter.rb
+++ app/models/gateway_parameter.rb
@@ -2,4 +2,12 @@ class GatewayParameter < ActiveRecord::Base
   attr_accessible :gateway_id, :name, :value, :class_type, :description
 
   belongs_to :gateway
+
+  validates :name,
+            :presence => true,
+            :uniqueness => true
+
+  validates :class_type,
+            :presence => true,
+            :inclusion => { :in => ['String', 'Integer', 'Boolean'] }
 end
