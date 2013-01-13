191649e6bad92940fc502582f8fcdd842807b8e1
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-01-13 22:55:06 +0100

    Restart FreeSWITCH after initial setup.

--- app/controllers/gemeinschaft_setups_controller.rb
+++ app/controllers/gemeinschaft_setups_controller.rb
@@ -46,6 +46,10 @@ class GemeinschaftSetupsController < ApplicationController
       GsParameter.where(:name => 'ringtone_url').first.update_attributes(:value => "http://#{@gemeinschaft_setup.sip_domain.host}")
       GsParameter.where(:name => 'user_image_url').first.update_attributes(:value => "http://#{@gemeinschaft_setup.sip_domain.host}/uploads/user/image")
 
+      # Restart FreeSWITCH
+      require 'freeswitch_event'
+      FreeswitchAPI.execute('fsctl', 'shutdown restart')
+
       # Auto-Login:
       session[:user_id] = user.id
       
