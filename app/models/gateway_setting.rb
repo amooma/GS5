857f3de83628c1de8d1c65ce2aea36ab7bd8e8c5
spag <spag@golwen.net>
2013-01-15 12:28:21 +0100

    gateway settings added

--- app/models/gateway_setting.rb
+++ app/models/gateway_setting.rb
@@ -1,7 +1,16 @@
 class GatewaySetting < ActiveRecord::Base
   CLASS_TYPES = ['String', 'Integer', 'Boolean']
   GATEWAY_SETTINGS = {
-    'sip' => { 'domain' => 'String', 'username' => 'String', 'password' => 'String', 'register' => 'Boolean', 'auth_source' =>  'String', 'auth_pattern' =>  'String' },
+    'sip' => { 
+      'domain' => 'String', 
+      'username' => 'String', 
+      'password' => 'String',
+      'contact' =>  'String',
+      'register' => 'Boolean', 
+      'auth_source' =>  'String', 
+      'auth_pattern' =>  'String',
+      'number_source' =>  'String',
+    },
   }
   
   attr_accessible :gateway_id, :name, :value, :class_type, :description
