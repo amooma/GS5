1d18e9d7fc16501d7622889df9d6ef05c75adb2f
spag <spag@golwen.net>
2013-01-17 06:59:08 +0100

    validations

--- app/models/call_route.rb
+++ app/models/call_route.rb
@@ -1,10 +1,16 @@
 class CallRoute < ActiveRecord::Base
+  ROUTING_TABLES = ['prerouting', 'outbound', 'inbound']
+
   attr_accessible :table, :name, :endpoint_type, :endpoint_id, :position
 
   has_many :route_elements, :dependent => :destroy
 
   validates :name,
   					:presence => true
+
+  validates :table,
+            :presence => true,
+            :inclusion => { :in => ROUTING_TABLES }
 
   acts_as_list :scope => '`table` = \'#{table}\''
 
