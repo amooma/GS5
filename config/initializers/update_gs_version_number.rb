313a6bfb2c5eddca7872bda007b7eae87306e5c6
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-01-18 19:06:01 +0100

    Update config/initializers/update_gs_version_number.rb

--- config/initializers/update_gs_version_number.rb
+++ config/initializers/update_gs_version_number.rb
@@ -2,5 +2,7 @@
 # It equals the branch in git.
 #
 if !ENV['GS_VERSION'].nil? && GsParameter.table_exists? && GsParameter.get('GEMEINSCHAFT_VERSION') != ENV['GS_VERSION']
-  GsParameter.where(:name => 'GEMEINSCHAFT_VERSION').first.update_attributes(:name => ENV['GS_VERSION'])
+  if GsParameter.where(:name => 'GEMEINSCHAFT_VERSION').any?
+    GsParameter.where(:name => 'GEMEINSCHAFT_VERSION').first.update_attributes(:name => ENV['GS_VERSION'])
+  end
 end
