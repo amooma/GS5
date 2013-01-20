fb18fd9bae2d24bef4652b40a4b0d206b77ebb4f
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-01-20 21:09:10 +0100

    Added breadcrumbs.

--- app/controllers/call_routes_controller.rb
+++ app/controllers/call_routes_controller.rb
@@ -1,22 +1,25 @@
 class CallRoutesController < ApplicationController
   authorize_resource :call_route
-  before_filter :spread_breadcrumbs
 
   def index
     @call_routes = CallRoute.order([:routing_table, :position])
     @routing_tables = @call_routes.pluck(:routing_table).uniq.sort
+    spread_breadcrumbs
   end
 
   def show
     @call_route = CallRoute.find(params[:id])
+    spread_breadcrumbs
   end
 
   def new
     @call_route = CallRoute.new
+    spread_breadcrumbs
   end
 
   def create
     @call_route = CallRoute.new(call_route_parameter_params)
+    spread_breadcrumbs
     if @call_route.save
       redirect_to @call_route, :notice => t('call_routes.controller.successfuly_created')
     else
@@ -26,10 +29,12 @@ class CallRoutesController < ApplicationController
 
   def edit
     @call_route = CallRoute.find(params[:id])
+    spread_breadcrumbs
   end
 
   def update
     @call_route = CallRoute.find(params[:id])
+    spread_breadcrumbs
     if @call_route.update_attributes(call_route_parameter_params)
       redirect_to @call_route, :notice  => t('call_routes.controller.successfuly_updated')
     else
@@ -61,7 +66,7 @@ class CallRoutesController < ApplicationController
   def spread_breadcrumbs
     add_breadcrumb t("call_routes.index.page_title"), call_routes_path
     if @call_route && !@call_route.new_record?
-      add_breadcrumb @call_route, call_route_path(@call_route)
+      add_breadcrumb @call_route, @call_route
     end
   end
 
