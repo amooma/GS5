e7968236a06c1be8c3ac23b5e63b7840659a334d
spag <spag@golwen.net>
2013-02-05 11:27:14 +0100

    added restart capablity

--- misc/freeswitch/scripts/event_manager.lua
+++ misc/freeswitch/scripts/event_manager.lua
@@ -7,12 +7,12 @@ require "common.log"
 local log = common.log.Log:new()
 log.prefix = "#E# "
 
-log:info('[event] EVENT_MANAGER start');
+log:info('[event] EVENT_MANAGER_LOADER start');
 
 require 'common.database'
 local database = common.database.Database:new{ log = log }:connect();
 if not database:connected() then
-  log:error('[event] EVENT_MANAGER - cannot connect to Gemeinschaft database');
+  log:error('[event] EVENT_MANAGER_LOADER - cannot connect to Gemeinschaft database');
   return;
 end
 
@@ -23,16 +23,21 @@ local domain = '127.0.0.1';
 if domains[1] then
   domain = domains[1]['host'];
 else
-  log:error('[event] EVENT_MANAGER - No SIP domains found!');
+  log:error('[event] EVENT_MANAGER_LOADER - No SIP domains found!');
 end
 
-require 'event.event'
-local event_manager = event.event.EventManager:new{ log = log, database = database, domain = domain }
-event_manager:run();
+freeswitch.setGlobalVariable('gs_event_manager', 'true');
+while freeswitch.getGlobalVariable('gs_event_manager') ~= 'false' do
+  package.loaded['event.event'] = nil;
+  local manager_class = require('event.event');
+  local event_manager = manager_class.EventManager:new{ log = log, database = database, domain = domain }
+  freeswitch.setGlobalVariable('gs_event_manager', 'true');
+  event_manager:run();
+end
 
 -- ensure database handle is released on exit
 if database then
   database:release();
 end
 
-log:info('[event] EVENT_MANAGER exit');
+log:info('[event] EVENT_MANAGER_LOADER exit');
