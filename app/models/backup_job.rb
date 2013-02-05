c7389cb6f1a305bc7e5cfefd9548c1e1bf7b6018
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-02-05 13:44:10 +0100

    Bugfix

--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -1,12 +1,12 @@
 class BackupJob < ActiveRecord::Base
   attr_accessible :started_at, :finished_at, :state, :directory, :size_of_the_backup
 
-  after_save :start_the_backup
+  after_create :start_the_backup
 
   private
   def start_the_backup
   	if self.finished_at.nil?
-      sh "backup perform --trigger gs5 --config_file #{Rails.root.join('config','backup.rb')}"
+      system "backup perform --trigger gs5 --config_file #{Rails.root.join('config','backup.rb')}"
       self.finished_at = Time.now
       self.save
     end
