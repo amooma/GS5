6891b0e62712c5c2772ef205b8393b7ed5e72495
spag <spag@golwen.net>
2013-02-12 23:29:55 +0100

    breadcrumbs

--- app/controllers/intruders_controller.rb
+++ app/controllers/intruders_controller.rb
@@ -1,24 +1,25 @@
 class IntrudersController < ApplicationController
   def index
     @intruders = Intruder.all
+    spread_breadcrumbs
   end
 
   def show
     @intruder = Intruder.find(params[:id])
     if ! params[:whois].blank?
       @whois = @intruder.whois(params[:whois])
-      if @whois.blank?
-        @whois = 'no information'
-      end
     end
+    spread_breadcrumbs
   end
 
   def new
     @intruder = Intruder.new
+    spread_breadcrumbs
   end
 
   def create
     @intruder = Intruder.new(params[:intruder])
+    spread_breadcrumbs
     if @intruder.save
       redirect_to @intruder, :notice => t('intruders.controller.successfuly_created')
     else
@@ -28,10 +29,12 @@ class IntrudersController < ApplicationController
 
   def edit
     @intruder = Intruder.find(params[:id])
+    spread_breadcrumbs
   end
 
   def update
     @intruder = Intruder.find(params[:id])
+    spread_breadcrumbs
     if @intruder.update_attributes(params[:intruder])
       redirect_to @intruder, :notice  => t('intruders.controller.successfuly_updated')
     else
@@ -41,7 +44,16 @@ class IntrudersController < ApplicationController
 
   def destroy
     @intruder = Intruder.find(params[:id])
+    spread_breadcrumbs
     @intruder.destroy
     redirect_to intruders_url, :notice => t('intruders.controller.successfuly_destroyed')
+  end
+
+  private
+  def spread_breadcrumbs
+    add_breadcrumb t("intruders.index.page_title"), intruders_path
+    if @intruder && !@intruder.new_record?
+      add_breadcrumb @intruder, @intruder
+    end
   end
 end
