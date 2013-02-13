8a496ffec7df055d31429076322cb767f1728c44
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-02-13 11:11:56 +0100

    Added a cronjob for the auto-reboot of phones. #169

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,13 @@
-# Use this file to easily define all of your cron jobs.
+# Daily Backup
 #
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 every 1.day, :at => '4:00 am' do
   rake "backup:daily_backup"
+end
+
+# Auto-Reboot of Phones which should be rebootet
+#
+every 1.day, :at => '2:30 am' do
+  command "/opt/GS5/script/logout_phones.sh"
 end
 
 # Learn more: http://github.com/javan/whenever
