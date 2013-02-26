774bb634d17190a30ed0e22183368ab46f74fe5d
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2013-02-26 18:18:12 +0100

    after_create :queue_the_restore_rake_task

--- app/models/restore_job.rb
+++ app/models/restore_job.rb
@@ -3,11 +3,24 @@ class RestoreJob < ActiveRecord::Base
 
   mount_uploader :backup_file, BackupFileUploader
 
+  after_create :queue_the_restore_rake_task
+
   def to_s
     if self.backup_file?
        File.basename(self.backup_file.to_s)
     else
       "RestoreJob ID #{self.id}"
     end
-  end  
+  end
+
+  private
+  def queue_the_restore_rake_task
+    self.delay.run_the_restore_rake_task
+  end
+
+  def run_the_restore_rake_task
+    system "cd #{Rails.root} && rake backup:restore"
+  end
+
+
 end
