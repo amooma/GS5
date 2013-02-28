1353b9c0563da02f1b17e54d802f28221cdce982
Peter Kozak <spag@golwen.net>
2013-02-28 09:02:19 -0500

    breadcrumbs

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,6 +3,7 @@ class CallsController < ApplicationController
   load_resource :call
 
   before_filter :set_and_authorize_parent
+  before_filter :spread_breadcrumbs
   
   def index
     if @parent
@@ -54,5 +55,21 @@ class CallsController < ApplicationController
   private
   def set_and_authorize_parent
     @parent = @sip_account
+  end
+
+  def spread_breadcrumbs
+    if @parent.class == SipAccount
+      if @sip_account.sip_accountable.class == User
+        add_breadcrumb t('users.name'), tenant_users_path(@sip_account.sip_accountable.current_tenant) 
+        add_breadcrumb @sip_account.sip_accountable, tenant_user_path(@sip_account.sip_accountable.current_tenant, @sip_account.sip_accountable) 
+        add_breadcrumb t('sip_accounts.index.page_title'), user_sip_accounts_path(@sip_account.sip_accountable) 
+        add_breadcrumb @sip_account, user_sip_account_path(@sip_account.sip_accountable, @sip_account) 
+        add_breadcrumb t('calls.index.page_title'), sip_account_calls_path(@sip_account) 
+      elsif @sip_account.sip_accountable.class == Tenant
+        add_breadcrumb t('sip_accounts.index.page_title'), tenant_sip_accounts_path(@sip_account.sip_accountable) 
+        add_breadcrumb @sip_account, tenant_sip_account_path(@sip_account.sip_accountable, @sip_account) 
+        add_breadcrumb t('calls.index.page_title'), sip_account_calls_path(@sip_account) 
+      end
+    end
   end
 end
