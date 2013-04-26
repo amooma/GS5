a94fafb695e19715505e05ab71034b14d7bde8c1
Peter Kozak <spag@golwen.net>
2013-04-26 12:14:55 +0200

    breadcrumbs added

--- app/controllers/voicemail_settings_controller.rb
+++ app/controllers/voicemail_settings_controller.rb
@@ -2,6 +2,8 @@ class VoicemailSettingsController < ApplicationController
   load_and_authorize_resource :voicemail_account
   load_and_authorize_resource :voicemail_setting, :through => [:voicemail_account]
 
+  before_filter :spread_breadcrumbs
+
   def index
     @voicemail_settings = @voicemail_account.voicemail_settings
   end
@@ -59,5 +61,22 @@ class VoicemailSettingsController < ApplicationController
     @voicemail_setting.destroy
     m = method( :"#{@voicemail_account.voicemail_accountable.class.name.underscore}_voicemail_account_path" )
     redirect_to m.( @voicemail_account.voicemail_accountable, @voicemail_account ), :notice => t('voicemail_settings.controller.successfuly_destroyed')
+  end
+
+  private
+  def spread_breadcrumbs
+    voicemail_accountable = @voicemail_account.voicemail_accountable
+    if voicemail_accountable.class == User
+      add_breadcrumb t("users.index.page_title"), tenant_users_path(voicemail_accountable.current_tenant)
+      add_breadcrumb voicemail_accountable, tenant_user_path(voicemail_accountable.current_tenant, voicemail_accountable)
+    end
+    
+    add_breadcrumb t("voicemail_accounts.index.page_title"), method( :"#{voicemail_accountable.class.name.underscore}_voicemail_accounts_url" ).(voicemail_accountable)
+    add_breadcrumb @voicemail_account.name, method( :"#{voicemail_accountable.class.name.underscore}_voicemail_account_path" ).(voicemail_accountable, @voicemail_account)
+    add_breadcrumb t("voicemail_settings.index.page_title"), voicemail_account_voicemail_settings_url(@voicemail_account)
+
+    if !@voicemail_setting.to_s.blank?
+      add_breadcrumb @voicemail_setting
+    end
   end
 end
