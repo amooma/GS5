0c545798470f64923db9b37b8abac96684c739eb
Peter Kozak <spag@golwen.net>
2013-04-26 12:25:27 +0200

    breadcrumbs

--- app/controllers/gateway_parameters_controller.rb
+++ app/controllers/gateway_parameters_controller.rb
@@ -2,6 +2,8 @@ class GatewayParametersController < ApplicationController
   load_and_authorize_resource :gateway
   load_and_authorize_resource :gateway_parameter, :through => [:gateway]
 
+  before_filter :spread_breadcrumbs
+
   def index
     @gateway_parameters = @gateway.gateway_parameters
   end
@@ -40,5 +42,16 @@ class GatewayParametersController < ApplicationController
     @gateway_parameter = @gateway.gateway_parameters.find(params[:id])
     @gateway_parameter.destroy
     redirect_to gateway_path(@gateway), :notice => t('gateway_parameters.controller.successfuly_destroyed')
+  end
+
+  private
+  def spread_breadcrumbs
+    add_breadcrumb t("gateways.index.page_title"), gateways_path
+    add_breadcrumb @gateway, @gateway
+    add_breadcrumb t("gateway_parameters.index.page_title"), gateway_gateway_parameters_url(@gateway)
+
+    if @gateway_parameter && !@gateway_parameter.new_record?
+      add_breadcrumb @gateway_parameter
+    end
   end
 end
