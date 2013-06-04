e4b3cf44d7f5a46cb051221b8b2c547a814943c9
Peter Kozak <spag@golwen.net>
2013-06-04 11:10:35 +0200

    call method added

--- app/models/pager_group_destination.rb
+++ app/models/pager_group_destination.rb
@@ -2,7 +2,14 @@ class PagerGroupDestination < ActiveRecord::Base
   attr_accessible :pager_group_id, :sip_account_id
 
   belongs_to :pager_group
+  belongs_to :sip_account
 
   validates_presence_of :pager_group_id
   validates_presence_of :sip_account_id
+
+  after_create :call
+
+  def call
+    self.sip_account.call("f-pager-#{self.pager_group_id}", '', "Pager #{self.id}")
+  end
 end
