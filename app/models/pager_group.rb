67b1a49f66be8051e7a6b550d416553b1df472f4
Peter Kozak <spag@golwen.net>
2013-06-04 13:09:25 +0200

    hangup_all method added

--- app/models/pager_group.rb
+++ app/models/pager_group.rb
@@ -7,8 +7,21 @@ class PagerGroup < ActiveRecord::Base
   validates_presence_of :sip_account_id
 
   after_create :call
+  before_destroy :hangup_all
+
+  def identifier
+    "pager#{self.id}"
+  end
 
   def call
     self.sip_account.call("f-pager-#{self.id}")
+  end
+
+  def hangup_all
+    require 'freeswitch_event'
+    return FreeswitchAPI.execute(
+      'conference', "#{self.identifier} hup all", 
+      true
+    );
   end
 end
