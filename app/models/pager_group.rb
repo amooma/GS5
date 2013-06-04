340cfdbf47096bf1263eee0803938ea6b8d605ab
Peter Kozak <spag@golwen.net>
2013-06-04 11:10:43 +0200

    call method added

--- app/models/pager_group.rb
+++ app/models/pager_group.rb
@@ -4,5 +4,11 @@ class PagerGroup < ActiveRecord::Base
   has_many :pager_group_destinations, :dependent => :destroy
   belongs_to :sip_account
 
-  validates_presence_of :sip_account_id 
+  validates_presence_of :sip_account_id
+
+  after_create :call
+
+  def call
+    self.sip_account.call("f-pager-#{self.id}")
+  end
 end
