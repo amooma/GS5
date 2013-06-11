aa7a4ebd68e1956fd8a0dbcfd4c8a617908ef47d
Peter Kozak <spag@golwen.net>
2013-06-11 14:53:48 +0200

    sip_cid_type added

--- app/models/gateway_setting.rb
+++ app/models/gateway_setting.rb
@@ -14,6 +14,7 @@ class GatewaySetting < ActiveRecord::Base
       'contact' =>  'String',
       'dial_string' =>  'String',
       'profile' =>  'String',
+      'sip_cid_type' => 'String',
     },
    'xmpp' => { 
       'server' => 'String', 
