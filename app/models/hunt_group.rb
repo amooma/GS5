3def6156141c1235d1ab0782900d43c097676243
Peter Kozak <spag@golwen.net>
2013-07-08 13:25:32 +0200

    destrination_call_forwards class_name added

--- app/models/hunt_group.rb
+++ app/models/hunt_group.rb
@@ -2,7 +2,7 @@ class HuntGroup < ActiveRecord::Base
   attr_accessible :name, :strategy, :seconds_between_jumps, :phone_numbers_attributes
 
   belongs_to :tenant, :touch => true
-  has_many :destrination_call_forwards, :as => :destinationable, :dependent => :destroy
+  has_many :destrination_call_forwards, :class_name => 'CallForward', :as => :destinationable, :dependent => :destroy
   has_many :call_forwards, :as => :call_forwardable, :dependent => :destroy
 
   validates_uniqueness_of :name, :scope => :tenant_id,
