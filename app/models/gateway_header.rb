90cbcb804676d70f0f6681ee50913221f387ad8a
Peter Kozak <spag@golwen.net>
2013-08-03 11:26:58 +0200

    gateway_headers model added

---
+++ app/models/gateway_header.rb
@@ -0,0 +1,24 @@
+class GatewayHeader < ActiveRecord::Base
+  HEADER_TYPES = [
+    'invite', 
+  #  'provisional', 
+  # 'request', 
+  #  'bye',
+  ]
+
+  attr_accessible :gateway_id, :header_type, :constraint_source, :constraint_value, :name, :value, :description
+
+  belongs_to :gateway, :touch => true
+
+  validates :name,
+            :presence => true,
+            :uniqueness => {:scope => [:gateway_id, :header_type, :constraint_source, :constraint_value]}
+
+  validates :header_type,
+            :presence => true,
+            :inclusion => { :in => HEADER_TYPES }
+
+  def to_s
+  	name
+  end
+end
